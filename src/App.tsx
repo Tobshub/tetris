c9c51fbfd10d2d1a016d5fac919d0ced0a2199fa
Tobshub <tobanigeorge@gmail.com>
2023-04-30 21:11:32 +0100

    change block shapes

--- src/App.tsx
+++ src/App.tsx
@@ -280,33 +280,33 @@ const BLOCKSHAPE = {
     [undefined, undefined, undefined, undefined],
   ],
   LBLOCK: [
-    [undefined, undefined, undefined, undefined],
     [new Square(), new Square(), new Square()],
     [new Square(), undefined, undefined],
+    [undefined, undefined, undefined, undefined],
   ],
   JBLOCK: [
-    [undefined, undefined, undefined],
     [new Square(), new Square(), new Square()],
     [undefined, undefined, new Square()],
+    [undefined, undefined, undefined],
   ],
   OBLOCK: [
     [new Square(), new Square()],
     [new Square(), new Square()],
   ],
   ZBLOCK: [
-    [undefined, undefined, undefined],
     [new Square(), new Square(), undefined],
     [undefined, new Square(), new Square()],
+    [undefined, undefined, undefined],
   ],
   SBLOCK: [
-    [undefined, undefined, undefined],
     [undefined, new Square(), new Square()],
     [new Square(), new Square(), undefined],
+    [undefined, undefined, undefined],
   ],
   TBLOCK: [
-    [undefined, undefined, undefined],
     [new Square(), new Square(), new Square()],
     [undefined, new Square(), undefined],
+    [undefined, undefined, undefined],
   ],
 };
 
